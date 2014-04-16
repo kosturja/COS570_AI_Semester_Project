79939b2f159cc524285de35498804ee97eead13b
Josh K <kosturja@gmail.com>
2014-04-15 23:50:02 -0400

    Just some basic stuff for the GO!

--- main.c
+++ main.c
@@ -4,6 +4,15 @@ int main(int argc, char *argv)
 {
 	//Need to change the file so I can commit it
 	//Another change mayber
+	//This will be the start of my AI semester project
+	//Which will be a GO! playing program
+
+	typedef struct{
+		int *board;
+		int which_player; //value to determine who's turn it is.
+		int *moves; // A location to store valid moves of the current board layout
+		
+	}Board
 	printf("Just a simple program that is really a place holder\n");
 	return 0;
 }
