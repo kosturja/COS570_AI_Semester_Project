6bd34537184727e9c35188fb64caad109c693f5d
Josh K <kosturja@gmail.com>
2014-04-15 23:44:04 -0400

    wtf

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv)
 {
 	//Need to change the file so I can commit it
-
+	//Another change mayber
 	printf("Just a simple program that is really a place holder\n");
 	return 0;
 }
